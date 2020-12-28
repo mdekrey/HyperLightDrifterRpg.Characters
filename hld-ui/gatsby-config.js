d2696b36375f6e8372e5e242fc2a4dbe3d734dfb
Matt DeKrey <mattdekrey@gmail.com>
2020-12-27 19:02:22 -0600

    Remove unused configuration

--- hld-ui/gatsby-config.js
+++ hld-ui/gatsby-config.js
@@ -1,24 +1,17 @@
 module.exports = {
-  siteMetadata: {
-    title: `DeKrey.NET`,
-    description: `Personal articles and profile of Matt DeKrey`,
-    author: `@mdekrey`,
-    url: `https://dekrey.net`,
-  },
-  plugins: [
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images-plugin`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+	siteMetadata: {
+		title: `DeKrey.NET`,
+		description: `Personal articles and profile of Matt DeKrey`,
+		author: `@mdekrey`,
+		url: `https://dekrey.net`,
+	},
+	plugins: [
+		`gatsby-plugin-postcss`,
+		`gatsby-plugin-react-helmet`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
+};
