ee4cd2224126b9abf38d90e5da4f5e5c0a2671c0
Matt DeKrey <mattdekrey@gmail.com>
2020-12-27 19:08:42 -0600

    Properly handle SSR

--- hld-ui/src/utils/useLocalstorageState.ts
+++ hld-ui/src/utils/useLocalstorageState.ts
@@ -1,6 +1,8 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { isSSR } from "./isSSR";
 
 export function useLocalstorageState<T>(key: string, initialState: T): [T, Dispatch<SetStateAction<T>>] {
+	if (isSSR()) return useState(initialState);
 	const localStorageRawValue = localStorage.getItem(key);
 	const [state, setState] = useState<T>(localStorageRawValue ? JSON.parse(localStorageRawValue) : initialState);
 	useEffect(() => {
